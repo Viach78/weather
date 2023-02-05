d54a4fbd619296a1cd5dc261ba632de32dad6fbb
Slava <ipvorobyev@mail.ru>
2023-02-05 14:25:43 +0300

    django_weather

--- weather/main/views.py
+++ weather/main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 
 def index(request):
+    appid = 'eeb4a90891d880640c4afdd29101473b'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city))
+
+    print(res.text)
     return render(request, 'main/index.html')
